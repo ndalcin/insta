aceae0f7e6e18e60d2bf4767efbe3d50366bb298
ndalcin <dalcin.nicole@gmail.com>
2018-07-24 22:35:32 -0400

    updated some tests

--- src/components/SearchBar/SearchBar.test.js
+++ src/components/SearchBar/SearchBar.test.js
@@ -4,11 +4,16 @@ import SearchBar from './SearchBar'
 
 describe('SearchBar', () => {
 	let searchBar
+	let query = ''
 	beforeEach(() => {
-		searchBar = shallow(<SearchBar />)
+		searchBar = shallow(<SearchBar query={query}/>)
 	})
 
-	it('should render a <div />', () => {
-		expect(searchBar.find('div').length).toEqual(1)
+	it('should render an <input />', () => {
+		expect(searchBar.find('input').length).toEqual(1)
+	})
+	it('should receive a prop from its parent that sets the value of the input', () => {
+
+		expect(searchBar.find('input').text()).toBe(query)
 	})
 })
--- src/containers/SearchContainer/SearchContainer.test.js
+++ src/containers/SearchContainer/SearchContainer.test.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import SearchContainer from './SearchContainer'
+import PostsList from '../../components/PostsList/PostsList'
 
 describe('SearchContainer', () => {
 	let searchContainer
 	beforeEach(() => {
-		searchContainer = shallow(<SearchContainer />)
+		searchContainer = shallow(<SearchContainer query=/>)
 	})
 
 	it('should render a <div />', () => {
 		expect(searchContainer.find('div').length).toEqual(1)
 	})
 	it('should render PostsList', () => {
-		expect(searchContainer.find('PostsList').exists()).toBe(true)
+		expect(searchContainer.find(PostsList).length).toEqual(1)
 	})
 	it('should render SearchBar', () => {
 		expect(searchContainer.find('SearchBar').exists()).toBe(true)
+	})
+	it('should initialize with a state with query:'' and posts:[]')
+
+	describe('user interactions', () => {
+		it('the query in state should change when a user types into the searchBar')
+		it('the posts in state should change when a user clicks on submit')
 	})
 })
